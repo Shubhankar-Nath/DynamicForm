4f92d538cef2addf7bf5dad186f265a5093af994
Shubhankar Nath <shnath@microsoft.com>
2019-09-24 23:20:19 +0530

    Add project files.

---
+++ DynamicForm/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using DynamicForm.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DynamicForm.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(List<PassengerModel> passengerModelList)
+        {
+            if( passengerModelList!= null) 
+            {
+                return View("PassengerView",passengerModelList);
+            }
+            return View();
+        }
+    }
+}
